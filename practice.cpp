efaea01d8665ffef32abc6fab3c3c9c892882c9e
Prasoonjha1 <57556301+Prasoonjha1@users.noreply.github.com>
2021-07-14 11:05:50 +0530

    Add files via upload

---
+++ practice.cpp
@@ -0,0 +1,7 @@
+#include <windows.h>
+
+int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
+{
+    return MessageBox(NULL, "hello, world", "MY first Program", 4);
+}
+ 
